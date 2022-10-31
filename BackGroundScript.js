fef2b1f60770dfd16309f841f8b22483d5aafb94
acbikash13 <acbikash13@gmail.com>
2022-10-31 15:24:11 -0400

    This is a test git commit

---
+++ BackGroundScript.js
@@ -0,0 +1,23 @@
+let y =  Math.floor(Math.random()*24 + 1);
+for(let i = 0; i< 10; i++){
+    console.log(Math.floor(Math.random()*24 + 1));
+}
+//let xy = prompt("Enter the size of the matrix: 5,7 or 9:  ")
+let boxes = document.getElementByTagName('td').innerHTML
+function myFunction(boxes) {
+    window.alert("bikash")
+}
+
+// Functionn when called will not alow changing of the numbers in the layout.
+function saveAll(){
+    const list = document.getElementsByTagName("tr");
+    for(let i = 0; i< list.length; i++){
+        list.getElementsByTagName("li").innerHTML = <li>getElementsByTagName[i]</li>;
+    }
+}
+//  When called everything inserted into the
+//fucntion confirmNumbersSetup()
+
+
+//
+
